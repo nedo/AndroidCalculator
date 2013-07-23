a43917baec43d78daa79f37f28e70906fc2e451e
naveed <luckynaveed@gmail.com>
2013-07-23 13:21:34 +0500

    Changes: 1. https://groups.google.com/forum/#!topic/robotium-developers/xevYdS1UHeU 2. SetOnTouchEvent with editfields to improve performance
    Issues Fixes
    1. Put '0' by default in both edit-fields
    2. Crash on tapping "Multiply" button with blank edit-fields

--- AndroidCalculator/src/com/calculator/Main.java
+++ AndroidCalculator/src/com/calculator/Main.java
@@ -4,6 +4,7 @@ import com.calculator.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,43 +12,63 @@ import android.widget.TextView;
 import android.text.Editable;
 
 public class Main extends Activity {
-	EditText FirstValue;
-	EditText SecondValue;
-	TextView Result;
-	Button Calculate;
-	float num1 , num2;	
+	EditText vFirstValue;
+	EditText vSecondValue;
+	TextView vResult;
+	Button vCalculate;
+	float vNum1 , vNum2, vOutPut;	
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        FirstValue = (EditText) findViewById(R.id.EditText01);
         
-        SecondValue = (EditText) findViewById(R.id.EditText02);
         
-        Result = (TextView) findViewById(R.id.TextView01);
-        Result.setText("0.00");
         
-        Calculate = (Button) findViewById(R.id.Button01);
+        vFirstValue = (EditText) findViewById(R.id.EditText01);
+        vFirstValue.setText("0");
+        vFirstValue.setOnTouchListener(new View.OnTouchListener() {
+			
+			public boolean onTouch(View v, MotionEvent event) {
+				vFirstValue.setText("");
+				return false;
+			}
+		});
+        
+        vSecondValue = (EditText) findViewById(R.id.EditText02);
+        vSecondValue.setText("0");
+        vSecondValue.setOnTouchListener(new View.OnTouchListener() {
+			
+			public boolean onTouch(View v, MotionEvent event) {
+				vSecondValue.setText("");
+				return false;
+			}
+        });
+         
+		
+        vResult = (TextView) findViewById(R.id.TextView01);
+        vResult.setText("0.00");
+        
+        vCalculate = (Button) findViewById(R.id.Button01);
 
         //Adding listener to button
-        Calculate.setOnClickListener(new View.OnClickListener() {
+        vCalculate.setOnClickListener(new View.OnClickListener() {
 
     	public void onClick(View v) {
     		//Getting first & second values and passing to show result
-    		showResult(FirstValue.getText(), SecondValue.getText());
+    		showResult(vFirstValue.getText(), vSecondValue.getText());
     	}
    	});
  }
 
  //Showing multiply results
- protected void showResult(Editable first, Editable second) 
+ protected void showResult(Editable vFirst, Editable vSecond) 
  {
-	 float num1 = Float.parseFloat(first.toString());
-	 float num2 = Float.parseFloat(second.toString());
-	 float result = num1 * num2;
-	 Result.setText(String.valueOf(result));
+	 vNum1 = Float.parseFloat(vFirst.toString());
+	 vNum2 = Float.parseFloat(vSecond.toString());
+	 vOutPut = vNum1 * vNum2;
+	 vResult.setText(String.valueOf(vOutPut));
  }
  
 }
